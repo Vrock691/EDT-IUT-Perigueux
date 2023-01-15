2c9311bde31133ba58d0b5f6f34727bd153c9682
Vrock691 <vrock691@gmail.com>
2023-01-15 17:31:53 +0100

    Login page started

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
+  bool pswvisible = false;
 
   @override
   Widget build(BuildContext context) {
@@ -45,61 +46,89 @@ class _LoginPageState extends State<LoginPage> {
         children: [
           Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  const Text(
-                    'Connexion à la plateforme',
-                    style: TextStyle(color: Colors.deepPurple, fontSize: 25),
-                  ),
-                  const Padding(padding: EdgeInsets.all(8.0)),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Entrez votre identifiant',
+            child: AutofillGroup(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const Text(
+                      'Connexion à la plateforme',
+                      style: TextStyle(color: Colors.deepPurple, fontSize: 25),
                     ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Veuillez entrer votre identifiant';
-                      }
-                      return null;
-                    },
-                  ),
-                  const Padding(padding: EdgeInsets.all(8.0)),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Entrez votre mot de passe',
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Veuillez entrer votre mot de passe';
-                      }
-                      return null;
-                    },
-                  ),
-                  const Padding(padding: EdgeInsets.all(8.0)),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(content: Text('Processing Data')),
-                            );
-                          }
-                        },
-                        child: const Padding(
-                          padding: EdgeInsets.all(16.0),
-                          child: Text('Se connecter'),
-                        ),
+                    const Padding(padding: EdgeInsets.all(8.0)),
+                    TextFormField(
+                      autofillHints: const [AutofillHints.username],
+                      decoration: const InputDecoration(
+                        labelText: 'Identifiant',
+                        border: OutlineInputBorder(),
+                        hintText: 'Entrez votre identifiant',
                       ),
-                    ],
-                  ),
-                ],
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Veuillez entrer votre identifiant';
+                        }
+                        return null;
+                      },
+                    ),
+                    const Padding(padding: EdgeInsets.all(8.0)),
+                    TextFormField(
+                      autofillHints: const [AutofillHints.password],
+                      obscureText: pswvisible,
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: InputDecoration(
+                          labelText: 'Mot de passe',
+                          border: const OutlineInputBorder(),
+                          hintText: 'Entrez votre mot de passe',
+                          suffix: IconButton(
+                              onPressed: () {
+                                //add Icon button at end of TextField
+                                setState(() {
+                                  //refresh UI
+                                  if (pswvisible) {
+                                    //if pswvisible == true, make it false
+                                    pswvisible = false;
+                                  } else {
+                                    pswvisible =
+                                        true; //if pswvisible == false, make it true
+                                  }
+                                });
+                              },
+                              icon: Icon(pswvisible == true
+                                  ? Icons.remove_red_eye
+                                  : Icons.password))),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Veuillez entrer votre mot de passe';
+                        }
+                        return null;
+                      },
+                    ),
+                    const Padding(padding: EdgeInsets.all(8.0)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton.icon(
+                          label: Text('Aide'),
+                          icon: Icon(Icons.help),
+                          onPressed: (() {}),
+                        ),
+                        ElevatedButton.icon(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text('Processing Data')),
+                              );
+                            }
+                          },
+                          icon: Icon(Icons.login),
+                          label: Text('Se connecter'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
