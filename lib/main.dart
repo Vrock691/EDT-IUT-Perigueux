ee89be25785bc4ea158bce9df35d4a081aa41b07
Vrock691 <vrock691@gmail.com>
2023-01-16 15:03:18 +0100

    login page upgrade

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,10 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
-  bool pswvisible = false;
+  bool pswinvisible = true;
+  bool isButtonEnabled = true;
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -58,6 +61,8 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                     const Padding(padding: EdgeInsets.all(8.0)),
                     TextFormField(
+                      enabled: isButtonEnabled,
+                      controller: usernameController,
                       autofillHints: const [AutofillHints.username],
                       decoration: const InputDecoration(
                         labelText: 'Identifiant',
@@ -73,8 +78,10 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                     const Padding(padding: EdgeInsets.all(8.0)),
                     TextFormField(
+                      enabled: isButtonEnabled,
+                      controller: passwordController,
                       autofillHints: const [AutofillHints.password],
-                      obscureText: pswvisible,
+                      obscureText: pswinvisible,
                       keyboardType: TextInputType.visiblePassword,
                       decoration: InputDecoration(
                           labelText: 'Mot de passe',
@@ -85,16 +92,16 @@ class _LoginPageState extends State<LoginPage> {
                                 //add Icon button at end of TextField
                                 setState(() {
                                   //refresh UI
-                                  if (pswvisible) {
-                                    //if pswvisible == true, make it false
-                                    pswvisible = false;
+                                  if (pswinvisible) {
+                                    //if pswinvisible == true, make it false
+                                    pswinvisible = false;
                                   } else {
-                                    pswvisible =
-                                        true; //if pswvisible == false, make it true
+                                    pswinvisible =
+                                        true; //if pswinvisible == false, make it true
                                   }
                                 });
                               },
-                              icon: Icon(pswvisible == true
+                              icon: Icon(pswinvisible == true
                                   ? Icons.remove_red_eye
                                   : Icons.password))),
                       validator: (value) {
@@ -109,21 +116,17 @@ class _LoginPageState extends State<LoginPage> {
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: [
                         TextButton.icon(
-                          label: Text('Aide'),
-                          icon: Icon(Icons.help),
-                          onPressed: (() {}),
-                        ),
+                            label: const Text('Aide'),
+                            icon: const Icon(Icons.help),
+                            onPressed: !isButtonEnabled
+                                ? null
+                                : () => _helpButtonAction()),
                         ElevatedButton.icon(
-                          onPressed: () {
-                            if (_formKey.currentState!.validate()) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                    content: Text('Processing Data')),
-                              );
-                            }
-                          },
-                          icon: Icon(Icons.login),
-                          label: Text('Se connecter'),
+                          onPressed: !isButtonEnabled
+                              ? null
+                              : () => _loginButtonAction(),
+                          icon: const Icon(Icons.login),
+                          label: const Text('Se connecter'),
                         ),
                       ],
                     ),
@@ -135,5 +138,51 @@ class _LoginPageState extends State<LoginPage> {
         ],
       ),
     );
+  }
+
+  void _loginButtonAction() {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        isButtonEnabled = false;
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Processing Data')),
+      );
+    }
+  }
+
+  void _helpButtonAction() {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Aide à la connexion',
+                  style: TextStyle(color: Colors.deepPurple, fontSize: 25),
+                ),
+                Padding(padding: EdgeInsets.all(8)),
+                Text(
+                  'Quels sont mes identifiants ?',
+                  style: TextStyle(color: Colors.deepPurple, fontSize: 20),
+                ),
+                Padding(padding: EdgeInsets.all(8)),
+                Text(
+                    "Vos identifiants sont les mêmes que ceux que vous entrez sur le site gpu.iut.bordeaux.fr, nous nous connectons à ce site avec ces informations pour obtenir votre emploi du temps."),
+                Padding(padding: EdgeInsets.all(8)),
+                Text(
+                  "Je n'arrive pas à me connecter.",
+                  style: TextStyle(color: Colors.deepPurple, fontSize: 20),
+                ),
+                Padding(padding: EdgeInsets.all(8)),
+                Text(
+                    "Avant toute choses, pensez à vérifier votre connexion internet, et à redemmarrer l'application ou votre appareil.\n\nDans un deuxième temps, essayez de vous connecter sur le site internet officiel Sattelys GPU pour vérifier si le problème ne vient pas d'eux. Ensuite vérifiez que vous avez bien rentré le bon identifiant et mot de passe.\n\nSi le problème persiste, vous pouvez nous contacter à l'adresse email suivante : vrock691@gmail.com. Ou sur instagram."),
+              ],
+            ),
+          );
+        });
   }
 }
